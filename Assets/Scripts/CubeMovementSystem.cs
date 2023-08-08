e77cd7a37628cfe100fec24f6425347dd88b89e2
Sokuhatiku <sokuhatiku@gmail.com>
2023-08-08 13:54:55 +0900

    キャラクターが移動方向を向くようにした

--- Assets/Scripts/CubeMovementSystem.cs
+++ Assets/Scripts/CubeMovementSystem.cs
@@ -15,11 +15,22 @@ public partial struct CubeMovementSystem : ISystem
     public void OnUpdate(ref SystemState state)
     {
         var speed = SystemAPI.Time.DeltaTime * 4;
+        var rotateSpeed = SystemAPI.Time.DeltaTime * 720;
+
         foreach (var (input, trans) in SystemAPI.Query<RefRO<CubeInput>, RefRW<LocalTransform>>().WithAll<Simulate>())
         {
             var moveInput = new float2(input.ValueRO.Horizontal, input.ValueRO.Vertical);
             moveInput = math.normalizesafe(moveInput) * speed;
             trans.ValueRW.Position += new float3(moveInput.x, 0, moveInput.y);
+            if (math.lengthsq(moveInput) > 0)
+            {
+                trans.ValueRW.Rotation =
+                    Quaternion.RotateTowards(
+                        trans.ValueRO.Rotation,
+                        quaternion.LookRotationSafe(new float3(moveInput.x, 0, moveInput.y), math.up()),
+                        rotateSpeed);
+                ;
+            }
         }
     }
 }
